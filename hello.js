201660eb17f07f5377aefc698f234e350c73bbdd
Dede Augustine <bookdedeaugustine@gmail.com>
2024-04-24 10:01:43 -0500

    added js

---
+++ hello.js
@@ -0,0 +1,16 @@
+//turn on strict mode to help us with best practices and error checking
+"use strict"
+
+//creating a variable called message to hold the message "Hello World"
+let message = "Hello World!";
+
+//displaying the message variable in the console
+console.log(message);
+
+
+let totalCheck = 58.45;
+let tipAmount =5.00;
+
+let completeTotal = totalCheck + tipAmount;
+
+console.log("your total after tip was"+ completeTotal;)
